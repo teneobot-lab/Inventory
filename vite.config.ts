6c6003ef0ea56342154c325b149de3f64be7f215
teneobot-lab <teneobot@gmail.com>
2026-01-29 13:10:06 +0700

    feat(api): Update proxy target to production server
    Changes the Vite development server's API proxy target from localhost to the production server's IP address. This ensures that the development environment correctly interacts with the deployed backend API.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -14,7 +15,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          target: 'http://localhost:3010',
+          target: 'http://159.223.57.240:3010',
           changeOrigin: true,
           secure: false,
         }
